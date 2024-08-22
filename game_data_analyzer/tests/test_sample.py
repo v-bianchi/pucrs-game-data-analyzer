a8ac4e7c6e8371dbb6409478cc8104dea972918f
Victor Bianchi <vbianchi@protonmail.com>
2024-08-22 19:41:23 -0300

    split tests

--- game_data_analyzer/tests/test_sample.py
+++ game_data_analyzer/tests/test_sample.py
@@ -1,12 +1,16 @@
 from game_data_analyzer.datasets.steam_game_data import SteamGameData
 
+games = SteamGameData(use_sample_data=True)
 
-def test_sample():
-  games = SteamGameData(use_sample_data=True)
+def test_question_1():
   free_paid_games_percent = games.free_paid_games_percent()
-  most_releases_year = games.most_releases_year()
-  linux_russian_games_percent = games.linux_russian_games_percent()
   assert free_paid_games_percent[0] == 9.523809523809524
   assert free_paid_games_percent[1] == 90.47619047619048
+
+def test_question_2():
+  most_releases_year = games.most_releases_year()
   assert most_releases_year == '2019'
+
+def test_question_3():
+  linux_russian_games_percent = games.linux_russian_games_percent()
   assert linux_russian_games_percent == 14.285714285714285
